ac3c6d031d07f4017e7ec15ac7bece4b83e0df72
Damian Chee <silverousmechs@gmail.com>
2024-04-29 19:48:21 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Display from "./components/Display";
+import ReportBuilder from "./components/ReportBuilder";
 
 function App() {
   return (
     <div>
       <h2>GA SEI</h2>
+      <ReportBuilder />
       <Display />
     </div>
   );
