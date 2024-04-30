e54c5233b6d50b5e7ae133166ba720cb20bfbe9f
Damian Chee <silverousmechs@gmail.com>
2024-05-01 06:19:57 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import Display from "./components/Display";
-import ReportBuilder from "./components/ReportBuilder";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Sidebar from "./components/Sidebar";
+
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import AdminHome from "./pages/AdminHome";
+import EmployerHome from "./pages/EmployerHome";
 
 function App() {
   return (
     <div>
       <h2>GA SEI</h2>
-      <ReportBuilder />
-      <Display />
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/employer" element={<EmployerHome />} />
+          <Route path="/adminhome" element={<AdminHome />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
