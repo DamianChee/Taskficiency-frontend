40d52d661c7a4ba9ca43dd689fe5a240dacaf0d0
Damian Chee <silverousmechs@gmail.com>
2024-05-01 06:24:34 +0800

    Create DevPage

---
+++ src/pages/DevPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReportBuilder from "../components/ReportBuilder";
+import Display from "../components/Display";
+
+const DevPage = () => {
+  return (
+    <div>
+      <ReportBuilder />
+      <Display />
+    </div>
+  );
+};
+
+export default DevPage;
