730f93a6e9351813ada6dce3aad52d52d8ab379c
Damian Chee <silverousmechs@gmail.com>
2024-05-01 06:19:27 +0800

    Create AdminHome.jsx

---
+++ src/pages/AdminHome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReportBuilder from "../components/ReportBuilder";
+import Display from "../components/Display";
+
+const AdminHome = () => {
+  return (
+    <div>
+      <ReportBuilder />
+      <Display />
+    </div>
+  );
+};
+
+export default AdminHome;
