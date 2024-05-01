d9ed87409ba1be49380574ed1992d0aa43478f67
Damian Chee <silverousmechs@gmail.com>
2024-05-02 01:20:14 +0800

    Update EmployerHome.jsx

--- src/pages/EmployerHome.jsx
+++ src/pages/EmployerHome.jsx
@@ -36,14 +36,17 @@ const EmployerHome = () => {
   return (
     <div className="container">
       <div className="row">
+        <h3 className="p-3 m-3 bg-info bg-opacity-10 border border-info rounded">
+          Employees
+        </h3>
         {users.map((item, key) => {
           return (
-            <div className="col-sm-10 mx-auto my-1">
-              <div class="card" key={key}>
-                <div class="card-header">{item.id}</div>
-                <div class="card-body">
-                  <h5 class="card-title">{item.name}</h5>
-                  <p class="card-text">
+            <div className="col-sm-10 mx-auto my-1" key={key}>
+              <div className="card" key={key}>
+                <div className="card-header">{item.id}</div>
+                <div className="card-body">
+                  <h5 className="card-title">{item.name}</h5>
+                  <p className="card-text">
                     username: {item.username} <br />
                     password: {item.password}
                   </p>
